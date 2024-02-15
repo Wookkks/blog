0ecd574071b168a93306d0b749bfa31adc6e0d77
Wookkks <discdao@gmail.com>
2024-02-15 12:31:52 +0900

    - RSA 완료 - DB 저장시 해시 암호화 적용 완료

--- src/main/java/com/example/seo/blog/controller/api/UserApiController.java
+++ src/main/java/com/example/seo/blog/controller/api/UserApiController.java
@@ -1,5 +1,6 @@
 package com.example.seo.blog.controller.api;
 
+import com.example.seo.blog.common.Hashing;
 import com.example.seo.blog.common.RSAUtil;
 import com.example.seo.blog.dto.ResponseDto;
 import com.example.seo.blog.dto.UserDto;
@@ -10,7 +11,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 
 @RestController
@@ -22,10 +22,21 @@ public class UserApiController {
 
     @PostMapping("/user/api/join")
     @ResponseBody
-    public ResponseDto join(@RequestBody User user) {
-        try{
-            userService.join(user);
-            log.info(user.toString());
+    public ResponseDto join(@RequestBody User user, HttpSession session) {
+        try {
+            PrivateKey privateKey = (PrivateKey) session.getAttribute(RSAUtil.PRIVATE_KEY);
+            String rawPassword = RSAUtil.decryptRSA(user.getPassword(), privateKey);
+            String salt = Hashing.getSalt();
+            String encPassword = Hashing.hashPassword(rawPassword, salt);
+
+            UserDto userDto = new UserDto();
+            userDto.setName(user.getName());
+            userDto.setUsername(user.getUsername());
+            userDto.setEmail(user.getEmail());
+            userDto.setPassword(encPassword);
+            userDto.setSalt(salt);
+
+            userService.join(userDto);
             return new ResponseDto("success", "회원가입이 완료되었습니다.");
         }catch (Exception e){
             return new ResponseDto("failure", "회원가입에 실패하였습니다.");
@@ -45,18 +56,12 @@ public class UserApiController {
         if(privateKey == null) {
             throw new RuntimeException("암호화 비밀키 정보를 찾을 수 없습니다.");
         }
-
         try {
             String username = userDto.getUsername();
-
-            String encryptedPassword = userDto.getPassword();
-            String decryptedPassword = RSAUtil.decryptRSA(userDto.getPassword(), privateKey);
-
-            log.info("username : " + username);
-            log.info("encryptedPassword : " + encryptedPassword);
-            log.info("decryptedPassword : " + decryptedPassword);
-
-            User loginUser = userService.findUser(username, encryptedPassword);
+            String salt = userService.findSalt(username);
+            String rawPassword = RSAUtil.decryptRSA(userDto.getPassword(), privateKey);
+            String encPassword = Hashing.hashPassword(rawPassword, salt);
+            User loginUser = userService.findUser(username, encPassword);
             session.setAttribute("userId", loginUser.getId());
             return new ResponseDto("success", loginUser.getName() + "님 환영합니다.");
 
@@ -69,24 +74,24 @@ public class UserApiController {
     @PutMapping("/user/api/update/{id}")
     @ResponseBody
     public ResponseDto userUpdate(@RequestBody UserDto userDto, @PathVariable int id) {
-        try{
+        try {
             User user = userService.findById(id).orElseThrow(()->new IllegalArgumentException("회원을 찾을 수 없습니다."));
             user.setName(userDto.getName());
             user.setEmail(userDto.getEmail());
             user.setPassword(user.getPassword());
             userService.update(user);
             return new ResponseDto("success", "수정이 완료되었습니다.");
-        }catch (Exception e){
+        } catch (Exception e) {
             return new ResponseDto("failure", "수정에 실패하였습니다.");
         }
     }
     @DeleteMapping("/user/api/withdraw/{id}")
     public ResponseDto userWithdraw(@PathVariable int id) {
-        try{
+        try {
             userService.findById(id).orElseThrow(()->new IllegalArgumentException("회원을 찾을 수 없습니다."));
             userService.withDraw(id);
             return new ResponseDto("success", "탈퇴가 완료되었습니다. 서비스를 이용해 주셔서 감사합니다.");
-        }catch (Exception e) {
+        } catch (Exception e) {
             return new ResponseDto("failure", "에러가 발생하였습니다. 관리자에게 문의해주세요.");
         }
     }
